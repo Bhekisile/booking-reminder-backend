41879ff0c7f8e8fbf9d9706f34450372ce1ce1a1
Bhekisile Jozi <bhekisilejozi@gmail.com>
2025-07-15 16:24:32 +0200

    Implement payfast controller and itn method

---
+++ app/controllers/api/v1/payfast_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PayfastController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def itn
+    user_id = params[:custom_str1]
+    user = User.find_by(id: user_id)
+
+    if params[:payment_status] == 'COMPLETE'
+      user.update(subscribed: true)
+      Rails.logger.info "PayFast: Subscription marked complete for User##{user.id}"
+    else
+      Rails.logger.warn "PayFast: Incomplete payment for User##{user_id}"
+    end
+
+    head :ok
+  end
+end
