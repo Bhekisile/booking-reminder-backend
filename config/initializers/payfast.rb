e75c38a3ef8915c02fb6eea738b3ca397fd3aff1
Bhekisile Jozi <bhekisilejozi@gmail.com>
2025-07-15 17:20:17 +0200

    Made changes to payfast configuration url

--- config/initializers/payfast.rb
+++ config/initializers/payfast.rb
@@ -1,8 +1,8 @@
 PAYFAST_CONFIG = {
   merchant_id: ENV['PAYFAST_MERCHANT_ID'],
   merchant_key: ENV['PAYFAST_MERCHANT_KEY'],
-  return_url: 'http://localhost:8081/subscription/SubscriptionSuccessScreen',
-  cancel_url: 'http://localhost:8081',
-  notify_url: 'http://localhost:3001/api/v1/payfast/itn',
+  return_url: 'https://booking-reminder.expo.app/subscription/SubscriptionSuccessScreen',
+  cancel_url: 'https://booking-reminder.expo.app',
+  notify_url: 'https://booking-reminder-backend.onrender.com/api/v1/payfast/itn',
   passphrase: ENV['PAYFAST_PASSPHRASE']
 }
