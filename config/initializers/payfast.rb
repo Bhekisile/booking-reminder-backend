ee1862c3afcf0e720ed02bec3189ac7822406ae8
Bhekisile Jozi <bhekisilejozi@gmail.com>
2025-07-18 23:27:20 +0200

    Fix bracket error

--- config/initializers/payfast.rb
+++ config/initializers/payfast.rb
@@ -5,3 +5,4 @@ PAYFAST_CONFIG = {
   cancel_url: 'https://booking-reminder.expo.app.link/home',
   notify_url: 'https://booking-reminder-backend.onrender.com/api/v1/payfast/itn',
   passphrase: ENV['PAYFAST_PASSPHRASE']
+}
